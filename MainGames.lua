eafe960489edec8dd23732372fa40ae0f8355c19
Faheem <faheemrayyan17@gmail.com>
2025-07-06 22:05:56 +0800

    Update MainGames.lua

--- MainGames.lua
+++ MainGames.lua
@@ -37,12 +37,11 @@ local Window = Rayfield:CreateWindow({
    }
 })
 
-local Tab = Info:CreateTab("Info", "badge-info")
+local Info = Window:CreateTab("Info", "badge-info")
+local Main = Window:CreateTab("Main", "house")
 
 local Paragraph = Info:CreateParagraph({Title = "★ Zeinon ", Content = "Main Owner Hub"})
 local Paragraph = Info:CreateParagraph({Title = "Join Our discord!", Content = "Join our discord to have some suggestions and features https://discord.gg/9JMzg2rp"})
-
-local Tab = Main:CreateTab("Main", "house")
 
 local Section = Main:CreateSection("LocalPlayer")
 
